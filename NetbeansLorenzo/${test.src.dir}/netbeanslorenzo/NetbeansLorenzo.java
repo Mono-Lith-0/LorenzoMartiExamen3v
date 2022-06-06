d63cb96cca8b37ded875f134afd8510a4ace7d50
Izan Lorenzo Martí <1770812@alu.murciaeduca.es>
2022-06-06 21:11:04 +0200

    commit para main

---
+++ NetbeansLorenzo/${test.src.dir}/netbeanslorenzo/NetbeansLorenzo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author izan
+ */
+public class NetbeansLorenzo {
+    
+    public static void fecha() {
+        
+        System.out.println("06/07/2022");
+    }
+    
+    public static void main(String[] args) {
+        
+        fecha();
+    }
+}
---
+++ NetbeansLorenzo/src/netbeanslorenzo/NetbeansLorenzo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author izan
+ */
+public class NetbeansLorenzo {
+    
+    public static void fecha() {
+        
+        System.out.println("06/06/2022");
+    }
+    
+    public static void Main(String[] args) {
+        
+        fecha();
+    }
+}
