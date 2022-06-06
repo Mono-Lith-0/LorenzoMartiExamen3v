d5071101a9152a54472922a8f75901ef79fa491e
Izan Lorenzo Martí <1770812@alu.murciaeduca.es>
2022-06-06 21:04:32 +0200

    Ejercicio 2, apartado 1

---
+++ NetbeansLorenzo/src/netbeanslorenzo/NetbeansLorenzo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package netbeanslorenzo;
+
+/**
+ *
+ * @author izan
+ */
+public class NetbeansLorenzo {
+
+    public static void imprimirNombreApellidos() {
+        System.out.println("Izan Lorenzo Martí");
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        imprimirNombreApellidos();
+    }
+    
+}
